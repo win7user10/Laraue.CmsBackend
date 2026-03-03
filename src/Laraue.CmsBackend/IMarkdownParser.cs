7f90939592c7eda26eb838d7a57f79e1d718006c
Ilya Belyansky <win7user10@yandex.ru>
2026-03-03 10:34:12 +0300

    show readable error on markdown fail

--- src/Laraue.CmsBackend/IMarkdownParser.cs
+++ src/Laraue.CmsBackend/IMarkdownParser.cs
@@ -1,4 +1,5 @@
 ﻿using Laraue.CmsBackend.Contracts;
+using Laraue.Interpreter.Common;
 using Laraue.Interpreter.Markdown;
 using Laraue.Interpreter.Markdown.Meta;
 
@@ -17,36 +18,43 @@ public class MarkdownParser(
     
     public ParsedMdFile Parse(ContentProperties contentSource)
     {
-        var result = markdownTranspiler.ToHtml(contentSource.Markdown);
-        var content = result.HtmlContent;
-        
-        // Read text
-        var links = result.InnerLinks;
-        var properties = ParseProperties(result.Headers);
-
-        var contentType = properties.Remove("type", out var contentTypeProperty)
-            ? contentTypeProperty.Value?.ToString() ?? ContentTypeRegistry.UndefinedContentType
-            : ContentTypeRegistry.UndefinedContentType;
-
-        // TODO - logical path generating can be in separated class. 
-        var logicalPath = contentSource.Id == IndexFileName
-            ? contentSource.Path
-            : new FilePath(contentSource.Path.Segments.Append(contentSource.Id).ToArray());
-
-        var fileName = contentSource.Id == IndexFileName
-            ? null
-            : contentSource.Id;
-            
-        return new ParsedMdFile
+        try
         {
-            FileName = fileName,
-            ContentType = contentType,
-            Content = content,
-            Properties = properties.Values,
-            PhysicalPath = contentSource.Path,
-            LogicalPath = logicalPath,
-            InnerLinks = links,
-        };
+            var result = markdownTranspiler.ToHtml(contentSource.Markdown);
+            var content = result.HtmlContent;
+        
+            // Read text
+            var links = result.InnerLinks;
+            var properties = ParseProperties(result.Headers);
+
+            var contentType = properties.Remove("type", out var contentTypeProperty)
+                ? contentTypeProperty.Value?.ToString() ?? ContentTypeRegistry.UndefinedContentType
+                : ContentTypeRegistry.UndefinedContentType;
+
+            // TODO - logical path generating can be in separated class. 
+            var logicalPath = contentSource.Id == IndexFileName
+                ? contentSource.Path
+                : new FilePath(contentSource.Path.Segments.Append(contentSource.Id).ToArray());
+
+            var fileName = contentSource.Id == IndexFileName
+                ? null
+                : contentSource.Id;
+            
+            return new ParsedMdFile
+            {
+                FileName = fileName,
+                ContentType = contentType,
+                Content = content,
+                Properties = properties.Values,
+                PhysicalPath = contentSource.Path,
+                LogicalPath = logicalPath,
+                InnerLinks = links,
+            };
+        }
+        catch (CompileException exception)
+        {
+            throw new MarkdownParserException($"Incorrect markdown: {exception.Message}", exception);
+        }
     }
 
     private static Dictionary<string, ParsedMdFileProperty> ParseProperties(MarkdownHeader[] headers)
